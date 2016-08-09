11dc56ce39411208f38d26f10785938043d55655
Ian Schoonover <iantskon@gmail.com>
2016-08-09 11:01:56 -0500

    Force user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # landing page/ home page
   get 'welcome' => 'welcome#index'
+  # devise user routes (signup, login, etc.)
+  devise_for :users
   # nested user's movies routes
   resources :users, only: [:show] do
-  	resources :movies, only: [:index, :show, :create, :destroy]
+    resources :movies, only: [:index, :show, :create, :destroy]
   end
   # search movies route
   get 'search' => 'movies#search' # search_path => /search
   # movie details route
   get 'details/:imdb_id' => 'movies#details', as: 'details' # details_path(:id) => /details/:id
-  # devise user routes (signup, login, etc.)
-  devise_for :users
 end
