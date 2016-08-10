ee1472ef1dda9c495923ef582b8c629e0cd95448
Ian Schoonover <iantskon@gmail.com>
2016-08-10 10:02:34 -0500

    Add edit functionality with security

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+	before_action :find_review, only: [:edit, :update, :destroy]
+
   def create
   	@movie = Movie.find(params[:movie_id])
   	@review = @movie.reviews.build(review_params)
@@ -13,15 +15,45 @@ class ReviewsController < ApplicationController
   end
 
   def edit
+  	if @review.user != current_user
+  		sign_out current_user
+  		redirect_to root_path
+  	end
   end
 
   def update
+  	if @review.user == current_user
+  		if @review.update(review_params)
+  			flash[:notice] = "Review updated successfully."
+  			redirect_to @review.movie
+  		else
+  			flash[:alert] = "Unable to update review."
+  			render :edit
+  		end
+  	else
+  		sign_out current_user
+  		redirect_to root_path
+  	end
+
   end
 
   def destroy
+  	@movie = @review.movie
+  	if @review.user == current_user
+  		@review.destroy
+  		flash[:alert] = "Review deleted successfully."
+  		redirect_to @movie
+  	else
+  		sign_out current_user
+  		redirect_to root_path
+  	end
   end
 
   private
+  	def find_review
+  		@review = Review.find(params[:id])
+  	end
+
   	def review_params
   		params.require(:review).permit(:body)
   	end
